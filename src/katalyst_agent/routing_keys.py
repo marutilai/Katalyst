67423e73d4232a058ae54a8a3d177c509aaf6ba4
Maruti Agarwal <maruti@maruti.com>
2025-06-03 14:00:38 -0400

    removed unused files

--- src/katalyst_agent/routing_keys.py
+++
@@ -1,9 +0,0 @@
-# Node-name routing keys
-NODE_TOOL_RUNNER       = "tool_runner"
-NODE_ADVANCE_POINTER   = "advance_pointer"
-NODE_REPLANNER         = "replanner"
-NODE_AGENT_REACT       = "agent_react"
-
-# Special finish keys
-FINISH_MAX_ITERATIONS  = "FINISH_MAX_ITERATIONS"
-FINISH_SUCCESSFUL_COMPLETION = "FINISH_SUCCESSFUL_COMPLETION"
